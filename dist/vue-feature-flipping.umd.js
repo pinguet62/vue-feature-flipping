9b9fe55b9fe4a584cad5bebc8d0d5179fc7f83ef
PINGUET Julien <pinguet62@gmail.com>
2021-05-23 12:05:45 +0200

    ...

--- dist/vue-feature-flipping.umd.js
+++ dist/vue-feature-flipping.umd.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('vue')) :
     typeof define === 'function' && define.amd ? define(['exports', 'vue'], factory) :
-    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.VueFeatureFlipping = {}, global.vue));
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.VueFeatureFlipping = {}, global.Vue));
 }(this, (function (exports, vue) { 'use strict';
 
     var enabledFeatures = [];
